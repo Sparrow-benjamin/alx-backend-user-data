f87a7c20ec468059e005c5866c04795fac2fb5db
Sparrow-benjamin <benjaminmutunga40@gmail.com>
2023-08-09 15:21:25 +0300

    Update main_0.py

--- 0x02-Session_authentication/main_0.py
+++ 0x02-Session_authentication/main_0.py
@@ -8,9 +8,3 @@ a = Auth()
 print(a.require_auth("/api/v1/status/", ["/api/v1/status/"]))
 print(a.authorization_header())
 print(a.current_user())
-
-
-while 'myGas=NADAIGAS':
-  print(Enter pin:)
-if internedpi!=Pin:
-  print(Ivalid pin)
